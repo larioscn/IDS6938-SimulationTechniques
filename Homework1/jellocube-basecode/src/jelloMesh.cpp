087857859b943934dd49ad035b1f4d1f74b22df5
larioscn <larios.cn@knights.ucf.edu>
2017-02-22 22:14:59 -0500

    add. edits to NIs and force (unfinished)

--- Homework1/jellocube-basecode/src/jelloMesh.cpp
+++ Homework1/jellocube-basecode/src/jelloMesh.cpp
@@ -419,7 +419,7 @@ void JelloMesh::CheckForCollisions(ParticleGrid& grid, const World& world)
 
 void JelloMesh::ComputeForces(ParticleGrid& grid)
 {
-	// Add external froces to all points
+	// Add external forces to all points
 	for (int i = 0; i < m_rows + 1; i++) {
 		for (int j = 0; j < m_cols + 1; j++) {
 			for (int k = 0; k < m_stacks + 1; k++) {
@@ -436,6 +436,13 @@ void JelloMesh::ComputeForces(ParticleGrid& grid)
 		Particle& b = GetParticle(grid, spring.m_p2);
 
 		// TODO
+		vec3 diff = a.position - b.position;
+		double dist = diff.Length();
+		if (dist != 0) {
+			vec3 force = m_Ks + m_Kd;    //FORCE EQUATION
+			 a.force += force;
+			b.force += -force;   //  Newtons 3rd law
+		}
 	}
 }
 
