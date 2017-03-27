2d0a6ab0130511f5f7e54df8c0765156bdaf8c1e
larioscn <larios.cn@knights.ucf.edu>
2017-03-27 12:05:23 -0400

    added equations part 3 - c

--- Homework2/queues/mm1_queue.cpp
+++ Homework2/queues/mm1_queue.cpp
@@ -76,12 +76,12 @@ void MM1_Queue::initialize()
     {
 
       //TODO---------------------------------------------
-      expected_server_utilization_ ;
-      expected_idle_prob_ ;
-      expected_queue_length_ ;
-      expected_number_customers_ ;
-      expected_waiting_time_ ;
-      expected_response_time_ ;
+      expected_server_utilization_ = (lambda_/mu_); 
+      expected_idle_prob_ = 1.0 - expected_server_utilization_;
+      expected_queue_length_ = ((expected_server_utilization_* expected_server_utilization_)/(expected_idle_prob_));
+      expected_number_customers_ = expected_server_utilization_ / expected_idle_prob_;
+      expected_waiting_time_ = expected_server_utilization_/ (lambda_ - mu_);
+      expected_response_time_ = 1.0 / (lambda_ - mu_);
       // ------------------------------------------------
 
       rnd_arrival.set_rate(lambda_);
